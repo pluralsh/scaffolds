afd76952856d42de70c237a680d0674d5e10eb52
Sebastian Florek <sebastian@plural.sh>
2025-01-24 14:04:41 +0100

    update osartifact

--- catalogs/edge/osbuilder/terraform/aws/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  workspace = yamldecode(data.local_sensitive_file.workspace.content)
-  subdomain = lookup(local.workspace.spec, "network", "subdomain")
-}
-
-data "local_sensitive_file" "workspace" {
-  filename = "${path.module}/../../workspace.yaml"
-}
