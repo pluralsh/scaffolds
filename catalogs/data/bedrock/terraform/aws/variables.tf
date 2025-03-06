10da1ec8eeac95bc25e5a4aca6d4a26aa0024303
John Blackwell <john.blackwell@treno.io>
2025-03-06 12:47:15 -0500

    fix variables

--- catalogs/data/bedrock/terraform/aws/variables.tf
+++ catalogs/data/bedrock/terraform/aws/variables.tf
@@ -1,15 +1,4 @@
-variable "namespace" {
-  description = "The Kubernetes namespace where the service account exists"
-  type        = string
-}
-
 variable "cluster_name" {
-  type    = string
-  default = "plural"
-}
-
-variable "aws_region" {
-  description = "The AWS region where Bedrock is deployed"
-  type        = string
-  default     = "us-east-1"
+  type = string
+  default = "{{ context.cluster }}"
 }
