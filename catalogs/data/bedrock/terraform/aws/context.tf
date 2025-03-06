7ac273a19bd4de2fd48b0778f7be3ea0e0a2a4f9
John Blackwell <john.blackwell@treno.io>
2025-03-06 16:31:37 -0500

    nil pointer fix secrets, remove unnecessary url

--- catalogs/data/bedrock/terraform/aws/context.tf
+++ catalogs/data/bedrock/terraform/aws/context.tf
@@ -5,15 +5,10 @@ data "plural_service_context" "mgmt" {
 locals {
   configuration    = jsondecode(data.plural_service_context.mgmt.configuration)
   eks_cluster_name = lookup(local.configuration, "cluster_name", "default-cluster")
-  oidc_issuer_url  = replace(lookup(local.configuration, "eks_cluster_oidc_issuer_url", ""), "https://", "")
 }
 
 output "eks_cluster_name_debug" {
   value = local.eks_cluster_name
-}
-
-output "oidc_issuer_url_debug" {
-  value = local.oidc_issuer_url
 }
 
 output "vpc_id_debug" {
