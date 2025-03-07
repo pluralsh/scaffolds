25ee38b32161967cbc21dfdb2f1c20c68d5c200a
John Blackwell <john.blackwell@treno.io>
2025-03-07 11:43:22 -0500

    pr fixes

--- catalogs/data/bedrock/terraform/aws/context.tf
+++ catalogs/data/bedrock/terraform/aws/context.tf
@@ -6,15 +6,3 @@ locals {
   configuration    = jsondecode(data.plural_service_context.mgmt.configuration)
   eks_cluster_name = lookup(local.configuration, "cluster_name", "default-cluster")
 }
-
-output "eks_cluster_name_debug" {
-  value = local.eks_cluster_name
-}
-
-output "vpc_id_debug" {
-  value = local.configuration["vpc_id"]
-}
-
-output "region_debug" {
-  value = local.configuration["region"]
-}
--- catalogs/data/bedrock/terraform/aws/outputs.tf
+++ catalogs/data/bedrock/terraform/aws/outputs.tf
@@ -1,3 +1,3 @@
-output "bedrock" {
+output "bedrock_role_arn" {
   value = module.assumable_role_bedrock.iam_role_arn
 }
