4ec790e517d6b40e7dbc7ee61710feb54bfc82d3
John Blackwell <john.blackwell@treno.io>
2025-03-11 16:07:29 -0400

    remove unnecessary aws folder

--- catalogs/data/bedrock/terraform/aws/context.tf
+++
@@ -1,8 +0,0 @@
-data "plural_service_context" "mgmt" {
-  name = "plrl/clusters/${var.cluster}"
-}
-
-locals {
-  configuration    = jsondecode(data.plural_service_context.mgmt.configuration)
-  eks_cluster_name = lookup(local.configuration, "cluster_name")
-}
--- catalogs/data/bedrock/terraform/aws/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "bedrock_role_arn" {
-  value = module.assumable_role_bedrock.iam_role_arn
-}
