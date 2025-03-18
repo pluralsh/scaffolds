a07e8b12c17efea5effcd1d62d4380f56c166f05
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-03-18 12:29:04 +0100

    add providers

---
+++ catalogs/data/airbyte/terraform/azure/versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.51.0, < 4.0"
+    }
+    plural = {
+      source = "pluralsh/plural"
+      version = ">= 0.2.1"
+    }
+  }
+}
+
+provider "plural" {}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
