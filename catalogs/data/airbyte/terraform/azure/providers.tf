d857688a1292b2aa4d98d0723907cab5465c744d
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-03-25 14:38:05 +0100

    use new context

--- catalogs/data/airbyte/terraform/azure/providers.tf
+++ catalogs/data/airbyte/terraform/azure/providers.tf
@@ -1,14 +1,24 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.51.0, < 4.0"
-    }
     plural = {
       source = "pluralsh/plural"
       version = ">= 0.2.1"
     }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.51.0, < 4.0"
+    }
   }
+}
+
+provider "plural" {}
+
+data "plural_service_context" "identity" {
+  name = "plrl/azure/identity"
+}
+
+locals {
+  configuration = jsondecode(data.plural_service_context.identity.configuration)
 }
 
 provider "azurerm" {
@@ -17,6 +27,11 @@ provider "azurerm" {
       prevent_deletion_if_contains_resources = false
     }
   }
-}
 
-provider "plural" {}
+  use_cli = false
+  use_oidc = true
+  oidc_token_file_path = "/var/run/secrets/azure/tokens/azure-identity-token"
+  subscription_id = local.configuration["subscription_id"]
+  tenant_id = local.configuration["tenant_id"]
+  client_id = local.configuration["client_id"]
+}
