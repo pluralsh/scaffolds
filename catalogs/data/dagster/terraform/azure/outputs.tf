11e593c437afa505d778a938f7f9dd86f67fa50e
michaeljguarino <mjg@plural.sh>
2025-05-12 17:58:35 -0400

    Setup Dagster for azure
    Need to still test this out e2e

---
+++ catalogs/data/dagster/terraform/azure/outputs.tf
@@ -0,0 +1,35 @@
+output "blobstore_client_id" {
+  value = azurerm_user_assigned_identity.dagster.client_id
+}
+
+output "storage_account_name" {
+  value = data.azurerm_storage_account.sa.name
+}
+
+output "container_name" {
+  value = azurerm_storage_container.dagster_logs.name
+}
+
+output "postgres_host" {
+  value = azurerm_postgresql_flexible_server.postgres.fqdn
+}
+
+output "postgres_password" {
+  value = random_password.db_password.result
+  sensitive = true
+}
+
+output "oidc_cookie_secret" {
+  value = random_password.oidc_cookie.result
+  sensitive = true
+}
+
+output "oidc_client_id" {
+  value = plural_oidc_provider.dagster.client_id
+  sensitive = true
+}
+
+output "oidc_client_secret" {
+  value = plural_oidc_provider.dagster.client_secret
+  sensitive = true
+}
