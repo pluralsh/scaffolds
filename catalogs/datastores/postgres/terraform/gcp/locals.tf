db73d6d1f6877365a3149bbcd823d33471f473bd
Sebastian Florek <s.florek91@gmail.com>
2025-05-15 15:14:46 +0200

    fix: update module.pg indexing in db_url format for PostgreSQL configuration

--- catalogs/datastores/postgres/terraform/gcp/locals.tf
+++ catalogs/datastores/postgres/terraform/gcp/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  db_url = format("postgresql://admin:%s@%s:5432/${var.name}", random_password.password.result, try(module.pg[0].private_ip_address, ""))
+  db_url = format("postgresql://admin:%s@%s:5432/${var.name}", random_password.password.result, try(module.pg.private_ip_address, ""))
   ctx_network = jsondecode(data.plural_service_context.network.configuration)
   ctx_mgmt = jsondecode(data.plural_service_context.mgmt.configuration)
 }
