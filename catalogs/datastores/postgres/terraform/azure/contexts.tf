ece9776c73c4fe028ca110af9e6d844dd96cb5f6
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-05-15 16:35:35 +0200

    add azure providers

--- catalogs/datastores/postgres/terraform/azure/contexts.tf
+++ catalogs/datastores/postgres/terraform/azure/contexts.tf
@@ -1,3 +1,7 @@
+data "plural_service_context" "identity" {
+  name = "plrl/azure/identity"
+}
+
 data "plural_service_context" "mgmt" {
   name = "plrl/clusters/mgmt"
 }
--- catalogs/datastores/postgres/terraform/azure/locals.tf
+++ catalogs/datastores/postgres/terraform/azure/locals.tf
@@ -1,4 +1,5 @@
 locals {
+  identity_context = jsondecode(data.plural_service_context.identity.configuration)
   cluster_context = jsondecode(data.plural_service_context.mgmt.configuration)
   resource_group_name = local.cluster_context.resource_group_name
 }
