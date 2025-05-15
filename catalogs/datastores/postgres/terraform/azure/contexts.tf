a50df27a670c8065145496ea2d1bcbb10b023cb6
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-05-15 16:31:20 +0200

    add azure context

---
+++ catalogs/datastores/postgres/terraform/azure/contexts.tf
@@ -0,0 +1,3 @@
+data "plural_service_context" "mgmt" {
+  name = "plrl/clusters/mgmt"
+}
---
+++ catalogs/datastores/postgres/terraform/azure/locals.tf
@@ -0,0 +1,4 @@
+locals {
+  cluster_context = jsondecode(data.plural_service_context.mgmt.configuration)
+  resource_group_name = local.cluster_context.resource_group_name
+}
