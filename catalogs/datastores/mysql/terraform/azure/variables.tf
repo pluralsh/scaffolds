dd49702a63bdd76caff4872f53747a61d8ae9c0a
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-05-21 15:16:43 +0200

    update config

--- catalogs/datastores/mysql/terraform/azure/variables.tf
+++ catalogs/datastores/mysql/terraform/azure/variables.tf
@@ -1,27 +1,27 @@
 variable "network" {
-  description = "Network that database should belong to."
+  description = "Network that database should belong to"
 }
 
 variable "name" {
-  description = "Name of the database."
+  description = "Name of the database"
 }
 
 variable "db_version" {
   type = string
   default = "5.7"
-  description = "The version of the MySQL Flexible Server to use. Possible values are 5.7, and 8.0.21. Changing this forces a new MySQL Flexible Server to be created."
+  description = "The version of the MySQL Flexible Server to use, changing this forces a new MySQL Flexible Server to be created"
 }
 
 variable "db_size_gb" {
   type = number
-  default = 64
-  description = "The max storage allowed for the MySQL Flexible Server. Possible values are between 20 and 16384."
+  default = 32
+  description = "The max storage allowed for the MySQL Flexible Server"
 }
 
 variable "db_sku" {
   type = string
-  default = "GP_Standard_D2ads_v5"
-  description = "The SKU Name for the MySQL Flexible Server."
+  default = "GP_Standard_D2ds_v4"
+  description = "The SKU Name for the MySQL Flexible Server"
 }
 
 variable "db_username" {
