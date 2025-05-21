9dc1d54d8ba7ac6d0184a27e34c1d574408b22f0
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-05-21 12:04:10 +0200

    fix outputs

--- catalogs/datastores/mysql/terraform/azure/outputs.tf
+++ catalogs/datastores/mysql/terraform/azure/outputs.tf
@@ -1,13 +1,13 @@
 output "db_host" {
-  value = azurerm_postgresql_flexible_server.postgres.fqdn
+  value = azurerm_mysql_flexible_server.mysql.fqdn
 }
 
 output "db_user" {
-  value = azurerm_postgresql_flexible_server.postgres.administrator_login
+  value = azurerm_mysql_flexible_server.mysql.administrator_login
   sensitive = true
 }
 
 output "db_password" {
-  value = azurerm_postgresql_flexible_server.postgres.administrator_password
+  value = azurerm_mysql_flexible_server.mysql.administrator_password
   sensitive = true
 }
