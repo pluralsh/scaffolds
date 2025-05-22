88e6a21f0029946f59a207c64b28d6d5da5c6402
Sebastian Florek <s.florek91@gmail.com>
2025-05-22 14:22:05 +0200

    feat: add Terraform configuration for MySQL datastore on GCP

---
+++ catalogs/datastores/mysql/terraform/gcp/db.tf
@@ -0,0 +1,63 @@
+resource "random_password" "password" {
+  length      = 20
+  min_lower   = 1
+  min_numeric = 1
+  min_upper   = 1
+  special     = false
+}
+
+resource "google_sql_database_instance" "mysql" {
+  name             = var.name
+  project          = local.cluster_context.project_id
+  database_version = var.db_version
+  region           = local.network_context.region
+
+  settings {
+    tier              = var.db_tier
+    availability_type = var.db_availability_type
+
+    backup_configuration {
+      enabled            = true
+      binary_log_enabled = true
+      start_time         = "00:00"
+
+      backup_retention_settings {
+        retained_backups = var.backup_retention_days
+      }
+    }
+
+    ip_configuration {
+      ipv4_enabled       = false
+      private_network    = data.google_compute_network.network.id
+      allocated_ip_range = google_compute_global_address.private_ip_alloc.name
+    }
+
+    disk_size = var.db_storage_size_gb
+    disk_type = var.db_storage_disk_type
+
+    database_flags {
+      name  = "character_set_server"
+      value = "utf8mb4"
+    }
+
+    database_flags {
+      name  = "collation_server"
+      value = "utf8mb4_unicode_ci"
+    }
+  }
+
+  deletion_protection = false
+}
+
+resource "google_sql_database" "database" {
+  name      = var.name
+  instance  = google_sql_database_instance.mysql.name
+  charset   = "utf8mb4"
+  collation = "utf8mb4_unicode_ci"
+}
+
+resource "google_sql_user" "user" {
+  name     = var.db_username
+  instance = google_sql_database_instance.mysql.name
+  password = random_password.password.result
+}
