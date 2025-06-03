cf6ee89037c6729279d71a98d87a58f6b911bd7b
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-06-03 17:01:06 +0200

    add outputs

---
+++ catalogs/data/airbyte/terraform/gcp/outputs.tf
@@ -0,0 +1,39 @@
+output "project_id" {
+  value = "" // TODO
+  sensitive = true
+}
+
+output "credentials_json" {
+  value = google_service_account_key.default.private_key
+  sensitive = true
+}
+
+output "credentials_json_b64" {
+  value = base64decode(google_service_account_key.default.private_key)
+  sensitive = true
+}
+
+output "postgres_host" {
+  value = module.pg.instance_ip_address
+}
+
+output "postgres_password" {
+  description = "The database password"
+  value       = random_password.password.result
+  sensitive   = true
+}
+
+output "oidc_cookie_secret" {
+  value = random_password.oidc_cookie.result
+  sensitive = true
+}
+
+output "oidc_client_id" {
+  value = plural_oidc_provider.airbyte.client_id
+  sensitive = true
+}
+
+output "oidc_client_secret" {
+  value = plural_oidc_provider.airbyte.client_secret
+  sensitive = true
+}
