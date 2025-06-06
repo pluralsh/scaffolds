80331cd661ddec0e26807e793c41246326631dcf
Sebastian Florek <s.florek91@gmail.com>
2025-06-06 14:01:34 +0200

    fix(gke): replace var.cluster_name with local.cluster_name

--- catalogs/data/dagster/terraform/gcp/gke.tf
+++ catalogs/data/dagster/terraform/gcp/gke.tf
@@ -1,7 +1,7 @@
 data "google_client_config" "provider" {}
 
 data "google_container_cluster" "cluster" {
-  name     = var.cluster_name
+  name     = local.cluster_name
   location = local.region
 }
 
